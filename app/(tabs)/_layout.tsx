ecf9b4b8fa239dfc332e47888eeffc3c90ec816a
Bolshiyanov <bolshiyanov@gmail.com>
2023-08-23 01:56:41 +0100

    Added svg icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,22 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+// import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
 
-import Colors from '@/constants/Colors';
+import Home from "@/assets/icons/house-solid.svg";
+import Heart from "@/assets/icons/heart-solid.svg";
+import City from "@/assets/icons/tree-city-solid.svg";
+
+import Colors from "@/constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+// function TabBarIcon(props: {
+//  name: React.ComponentProps<typeof FontAwesome>['name'];
+//  color: string;
+// }) {
+//  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+// }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,21 +24,23 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Tab One",
+          tabBarIcon: () => (
+            <Home width={28} height={28} fill={Colors[colorScheme ?? "light"].text}
+              style={{ marginLeft: 5 }}
+            />
+          ),
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                {({ pressed }) => ( <Heart width={28} height={28}
+                    fill={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -46,8 +52,11 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Tab Two",
+          tabBarIcon: () => (
+            <City width={28} height={28} fill={Colors[colorScheme ?? "light"].text}
+            />
+          ),
         }}
       />
     </Tabs>
